12fc0560d55fb2062a439c0ccc4406242179276e
Vincent Ambo <mail@tazj.in>
2025-05-04 15:38:58 +0300

    chore(tvix): remove remaining feature powerset stuff
    Change-Id: I2bef488f25453d1b4d2b13f47ed191eab41e705c
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/13376
    Tested-by: BuildkiteCI
    Reviewed-by: sterni <sternenseemann@systemli.org>

--- utils.nix
+++ utils.nix
@@ -1,26 +1,6 @@
 { pkgs, lib, depot, ... }:
 
 {
-  mkFeaturePowerset = { crateName, features, override ? { } }:
-    let
-      powerset = xs:
-        let
-          addElement = set: element:
-            set ++ map (e: [ element ] ++ e) set;
-        in
-        lib.foldl' addElement [ [ ] ] xs;
-    in
-    lib.listToAttrs (map
-      (featuresPowerset: {
-        name = if featuresPowerset != [ ] then "with-features-${lib.concatStringsSep "-" featuresPowerset}" else "no-features";
-        value = depot.tvix.crates.workspaceMembers.${crateName}.build.override (old: {
-          runTests = true;
-          features = featuresPowerset;
-        } // (if lib.isFunction override then override old else override)
-        );
-      })
-      (powerset features));
-
   # Filters the given source, only keeping files related to the build, preventing unnecessary rebuilds.
   # Includes src in the root, all other .rs files and optionally Cargo specific files.
   # Additional files to be included can be specified in extraFileset.
