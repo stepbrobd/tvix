0c08debd09b5aff08ad47b626b9f7e2f1565ccd1
sterni <sternenseemann@systemli.org>
2025-11-02 15:04:13 +0100

    test(tvix/eval): sync nixVersion check with Nixpkgs
    Change-Id: Ieda9cbe9370720c576f607a9770b73f68ab79e30
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/13834
    Tested-by: BuildkiteCI
    Autosubmit: sterni <sternenseemann@systemli.org>
    Reviewed-by: tazjin <tazjin@tvl.su>

--- eval/src/tests/tvix_tests/eval-okay-nix-version-cmp.nix
+++ eval/src/tests/tvix_tests/eval-okay-nix-version-cmp.nix
@@ -2,4 +2,4 @@
 # doesn't like what it sees. To work around this we have a "user-agent
 # style" version (see cl/6858) that ensures compatibility.
 
-builtins.compareVersions "2.3" builtins.nixVersion
+builtins.compareVersions "2.18" builtins.nixVersion
